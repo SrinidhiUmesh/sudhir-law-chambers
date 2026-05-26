ac206f42c8eb10e497a4815e1ad37464449e8fc1
SrinidhiUmesh <srinidhiumesh@gmail.com>
2026-05-26 19:38:48 +0530

    Include Google site verification in _app.js
    Added Google site verification meta tag to the head.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,20 @@
 // pages/_app.js
-import '../styles/globals.css';   // ✅ Global CSS goes here
+
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+  return (
+    <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="C0S0tB8-hwxtGAqpCez6GHVOB5XyDvE7E9jFYJfvf-Y"
+        />
+      </Head>
 
+      <Component {...pageProps} />
+    </>
+  );
+}
 export default MyApp;
