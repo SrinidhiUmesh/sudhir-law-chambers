59ffb627f5a0f538981572c1db8f54229df5f881
SrinidhiUmesh <srinidhiumesh@gmail.com>
2026-05-26 19:35:29 +0530

    have to remove the above update

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,5 @@
 import Link from "next/link";
 
-<head>
-  <meta name="google-site-verification" content="C0S0tB8-hwxtGAqpCez6GHVOB5XyDvE7E9jFYJfvf-Y" />
-</head>
-
 export default function Navbar() {
   const toggleMenu = () => {
     if (typeof document === "undefined") return;
